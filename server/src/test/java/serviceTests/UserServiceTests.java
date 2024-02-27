df24205a013863daefcbdebdd7208ffe53aba06a
Reed Zitting <reedgzitting@gmail.com>
2024-02-26 17:38:51 -0700

    created login and register tests

--- server/src/test/java/serviceTests/UserServiceTests.java
+++ server/src/test/java/serviceTests/UserServiceTests.java
@@ -1,17 +1,49 @@
 package serviceTests;
 
+import dataAccess.MemoryAuthDAO;
+import dataAccess.MemoryUserDAO;
 import model.UserData;
+import org.eclipse.jetty.server.Authentication;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import requests.LoginRequest;
+import responses.LoginResponse;
+import responses.RegisterResponse;
 import service.ClearAppService;
 import service.UserService;
 
 public class UserServiceTests {
 
     @Test
-    public void registerUserTest(){
+    public void registerNegativeUserTest(){
+        new ClearAppService().clearAll();
+        UserData userData = new UserData(null, "password", "email");
+        RegisterResponse response = new UserService().register(userData);
+        Assertions.assertEquals(response.message(), "Error: bad request");
+    }
+    @Test
+    public void registerPositiveUserTest(){
         new ClearAppService().clearAll();
         UserData userData = new UserData("username", "password", "email");
+        RegisterResponse response = new UserService().register(userData);
+        Assertions.assertNull(response.message());
+        Assertions.assertEquals(response.username(), "username");
 
-        new UserService().register(userData);
+    }
+
+    @Test
+    public void loginPositiveUserTest(){
+        new ClearAppService().clearAll();
+        UserData userData = new UserData("name", "password", "email");
+        new MemoryUserDAO().createUser(userData);
+        LoginRequest request = new LoginRequest("name", "password");
+        LoginResponse response = new UserService().login(request);
+        Assertions.assertNull(response.message());
+        Assertions.assertEquals(response.authToken(), new MemoryAuthDAO().getAuth(response.authToken()));
+
+    }
+    @Test
+    public void loginNegativeUserTest(){
+        new ClearAppService().clearAll();
     }
 }
