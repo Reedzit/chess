514bae4f0a2b7424f937269288608104541964c7
Reed Zitting <reedgzitting@gmail.com>
2024-03-04 17:16:51 -0700

    fixed dependencies and automatically creates user table if empty

--- server/src/main/java/dataAccess/DbUserDAO.java
+++ server/src/main/java/dataAccess/DbUserDAO.java
@@ -1,16 +1,46 @@
 package dataAccess;
 
 import model.UserData;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.sql.SQLException;
 
 public class DbUserDAO  implements UserDAO{
+    String statement = """
+            CREATE TABLE IF NOT EXISTS  users (
+              `id` int NOT NULL AUTO_INCREMENT,
+              `username` varchar(256) NOT NULL,
+              `password` varchar(256) NOT NULL,
+              `email` TEXT DEFAULT NULL,
+              PRIMARY KEY (`id`),
+              INDEX(email),
+              INDEX(username)
+            )
+            """;
+    public DbUserDAO() throws DataAccessException{
+        configTable();
+    }
     @Override
     public void createUser(UserData entry) {
 
     }
 
+    public String encodePassword(String password){ return new BCryptPasswordEncoder().encode(password);}
+
     @Override
     public UserData getUser(String username) {
         return null;
+    }
+
+    public void configTable() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()){
+            try(var preparedStatement = conn.prepareStatement(statement)){
+                preparedStatement.executeUpdate();
+            }
+        }catch (SQLException ex){
+            throw new DataAccessException(String.format("Unable to configure database: %s", ex.getMessage()));
+        }
     }
 
     @Override
