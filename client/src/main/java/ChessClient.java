c8762edfcf566cdd95776f9145c85e8a852073c7
Reed Zitting <reedgzitting@gmail.com>
2024-03-22 07:51:30 -0600

    Added more content to ChessClient

--- client/src/main/java/ChessClient.java
+++ client/src/main/java/ChessClient.java
@@ -1,2 +1,13 @@
+import com.google.gson.Gson;
+import server.ServerFacade;
+
+
 public class ChessClient {
+    private final ServerFacade server;
+    private final String serverUrl;
+
+    public ChessClient(String serverUrl) {
+        server = new ServerFacade(serverUrl);
+        this.serverUrl = serverUrl;
+    }
 }
